79d6d1f76075a0ab9349d6d9e1b5c339b83dc165
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2021-03-24 15:39:11 +0900

    NodeJS Express

--- NodeBasic/lib/template.js
+++ NodeBasic/lib/template.js
@@ -19,7 +19,7 @@ module.exports = {
         var i=0;
         while(i < filelist.length){
             list = list + `<li><a
-            href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            href="/page/${filelist[i]}">${filelist[i]}</a></li>`;
             i=i+1;
         }
         list = list + '</ul>';
@@ -27,3 +27,34 @@ module.exports = {
     }
 }
 
+
+
+/*module.exports = {
+    HTML:function(title, list, body, control){
+        return `
+    <!doctype html>
+    <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <a href="/"><h1>WEB</h1></a>
+            ${list}
+            ${control}      
+            ${body}
+        </body>
+    </html>`;
+    }, list:function(filelist){
+        var list = '<ul>';
+        var i=0;
+        while(i < filelist.length){
+            list = list + `<li><a
+            href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i=i+1;
+        }
+        list = list + '</ul>';
+        return list;
+    }
+}
+*/

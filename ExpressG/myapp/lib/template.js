71046fd13bb3a86446237aa817d7c235110a6390
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2021-04-08 12:01:48 +0900

    페이징 기능 구현. 비효율적인 문제가 있음

--- ExpressG/myapp/lib/template.js
+++ ExpressG/myapp/lib/template.js
@@ -3,6 +3,7 @@ module.exports = {
         authStatusUI = '<a href="/auth/login">로그인</a>  | <a href="/customer/register">회원가입</a>'){
         return `
     <!doctype html>
+    <link rel="icon" href="data:,">
     <html>
         <head>
             <title>WEB1 - ${title}</title>
@@ -16,47 +17,44 @@ module.exports = {
             ${body}
         </body>
     </html>`;
-    }, list:function(filelist){
-        var list = '<ul>';
-        var i=0;
-        while(i < filelist.length){
-            list = list + `<li><a
-            href="/topic/${filelist[i].id}">${filelist[i].title}</a></li>`;
-            i=i+1;
-        }
-        list = list + '</ul>';
+    }, page:function(filelist){
+        var pagelength = filelist.length;
+        var j=1;
+        var page = `<a href="/${j}">[${j}] </a>`;
+        var maxpost = 3;
+        for(var i=0; i < filelist.length; i++){
+            if( pagelength > maxpost ){
+                    j++;
+                    page = page + `<a href="/${j}">[${j}]</a>`;
+                    pagelength -=maxpost;    
+                }  
+            }
+        return page
+
+    }, list:function(filelist, pageId){
+        var list = '<ol>';
+        var page = this.page(filelist);
+        var maxpost=3;
+
+        for(var i = 1 ; i <= filelist.length/maxpost +1 ; i++)
+            if(pageId === `${i}`){
+                var next = maxpost*i -maxpost;
+                for(var j=0 ; j<3 ; j++){
+                    list = list + `<li><a
+                    href="/topic/${filelist[j+next].id}">${filelist[j+next].title}</a></li>`;
+                    if(filelist.length % maxpost === j+1 && i === parseInt(filelist.length/maxpost) +1){
+                        break;
+                    }   
+                }
+            }else if(!pageId){
+                for(var j=0 ; j<3 ; j++){
+                    list = list + `<li><a
+                    href="/topic/${filelist[j].id}">${filelist[j].title}</a></li>`;
+                }
+                break;
+            }
+        
+        list = list + '</ol>' + page +'<br>';
         return list;
     }
 }
-
-
-
-/*module.exports = {
-    HTML:function(title, list, body, control){
-        return `
-    <!doctype html>
-    <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <a href="/"><h1>WEB</h1></a>
-            ${list}
-            ${control}      
-            ${body}
-        </body>
-    </html>`;
-    }, list:function(filelist){
-        var list = '<ul>';
-        var i=0;
-        while(i < filelist.length){
-            list = list + `<li><a
-            href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i=i+1;
-        }
-        list = list + '</ul>';
-        return list;
-    }
-}
-*/
--- ExpressG/myapp/routes/index.js
+++ ExpressG/myapp/routes/index.js
@@ -50,4 +50,32 @@ router.get('/', (request, response) => {
     }) 
 });
 
+//page
+router.get('/:pageId', (request, response, next) => {   
+    const topicquery = new Query(`SELECT * FROM topics`);
+    client.query(topicquery, (err, res) => {
+        var title = 'Node.js 게시판';
+        var description = `<a href="https://github.com/LEEHYUNHO2001/web1/tree/master" 
+                            target="_blank" title="github 주소">클릭 GitHub</a>`;
+
+        //template.list와 함께 생각해보기
+        /*if(request.params.pageId === 1){
+            list = template.list(res.rows)
+        }*/
+        /*
+        if(res.rows.length > 3){
+            console.log(res.rows[0].title)
+        }*/
+        
+        var list = template.list(res.rows, request.params.pageId);
+        var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}
+            <img src="/images/view.jpg" style="width:300px; display:block; margin-top:10px">`,
+            `<a href="/topic/create">글쓰기</a>`,
+            login.authStatusUI(request, response)
+            ); 
+        response.send(html); 
+    }) 
+});
+
 module.exports = router;

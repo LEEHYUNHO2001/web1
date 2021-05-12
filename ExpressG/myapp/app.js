d3c4445e27ce231a6930c991602468659645debe
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2021-05-12 10:07:39 +0900

    index,homeelse,loginStatus,head에 부트스트랩

--- ExpressG/myapp/app.js
+++ ExpressG/myapp/app.js
@@ -12,6 +12,9 @@ var bodyParser = require('body-parser');
 
 app.use(helmet()); //secure
 
+//app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+//app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
 var loginRouter = require('./routes/loginSession');

0e7c904821336bb5072243c2858e0ac710ced315
dlgusgh200240 <dlgusgh200240@gmail.com>
2021-05-24 11:13:28 +0900

    TDD - Node.js Express

---
+++ node-tdd/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+module.exports = app.listen(3000, () => {
+  console.log('Awesome TDD + ES6 app listening on port 3000!');
+});
